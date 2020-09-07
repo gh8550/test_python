843ddc03974dd273bf147d73c2f416c43f8eafa6
JUNHO <gh8550@gmail.com>
2020-09-07 22:18:15 +0900

    숙제완료

---
+++ q2_2.py
@@ -0,0 +1,7 @@
+#홀짝 구분
+temp = 21378
+if temp%2 == 0:
+    print(temp, '는 짝수입니다.')
+else:
+    print(temp, '는 홀수입니다.')
+
--- test1.py
+++ test1.py
@@ -51,7 +51,7 @@ for a,b temp:
 #     else:
 #         print("낙제")
 
-#선생님 방식ㅇ로 한번해보기
+# # 선생님 방식ㅇ로 한번해보기
 # test = [91, 80, 72, 64, 59]
 # for i, score in enumerate(test):
 #     if score >= 90:
@@ -66,5 +66,44 @@ for a,b temp:
 #         print("낙제")
 
 
-score = [x for x in range(0,101,10)]
-print(score)
+# score = [x for x in range(0,101,10)]
+# print(score)
+
+
+# treeHit = 0
+# while treeHit < 10:
+#     treeHit = treeHit +1
+#     print("나무를 %d번 찍었습니다." %treeHit)
+#     if treeHit == 10:
+#         print("나무 넘어갑니다")
+#
+#
+# prompt = """
+# 1. Add
+# 2. Del
+# 3. List
+# 4. Quit
+#
+# Enter number"""
+#
+# number = 0
+# while number != 4:
+#     print(prompt)
+#     number = int(input())
+
+
+coffee= 10
+while True:
+    money = int(input("돈을 넣어주세요 : "))
+    if money == 300:
+        print("커피를 줍니다")
+        coffee = coffee - 1
+    elif money >300:
+        print("거스름돈 %d를 주고 커피를 줍니다." %(money-300))
+        coffee = coffee-1
+    else:
+        print("돈을 다시 돌려주고 커피를 주지 않습니다.")
+        print("남은 커피의 양은 %d개 입니다" %coffee)
+    if coffee == 0:
+        print("커피가 다 떨어졌습니다. 판매를 중지 합니다.")
+        break
